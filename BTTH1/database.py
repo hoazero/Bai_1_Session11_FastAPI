7a653e38c737486cec287cd955159e995979618d
hoazero <hoadzhkb@gmail.com>
2026-07-08 22:31:10 +0700

    Add files via upload

--- BTTH1/database.py
+++ BTTH1/database.py
@@ -1,11 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-import urllib.parse
-
-password = urllib.parse.quote_plus("minhduy@3304")
-
-DB_URL = f"mysql+pymysql://root:{password}@localhost:3306/parking_db"
+DB_URL = "mysql+pymysql://root:123456@localhost:3306/parking_db"
 
 engine = create_engine(DB_URL)
 
